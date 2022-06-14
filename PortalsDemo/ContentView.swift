3f7f863a1b9f100b574f068593ef17f39443f11b
John Jusayan <john@thesufferfest.com>
2022-06-14 15:26:35 -0700

    Initial Commit

---
+++ PortalsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PortalsDemo
+//
+//  Created by John Jusayan on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PortalsDemo/PortalsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalsDemoApp.swift
+//  PortalsDemo
+//
+//  Created by John Jusayan on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PortalsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
